29aaf5cb6c8435dbdf7933863f1fcdef387f4b8f
Oleg Tervo-Ridor <oleg.tervo-ridor@helsinki.fi>
2021-02-13 15:10:30 +0200

    Full database

--- database/schema.sql
+++ database/schema.sql
@@ -19,3 +19,42 @@ CREATE TABLE IF NOT EXISTS Item (
 	author SERIAL,
 	FOREIGN KEY (author) REFERENCES Users(id)
 );
+
+CREATE TABLE IF NOT EXISTS ItemType (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(128) NOT NULL
+);
+
+INSERT INTO ItemType (id, name)
+values (0, 'Undefined'),
+(1, 'Väline'),
+(2, 'Ajoneuvo'),
+(3, 'Pinta ala'),
+(4, 'Palvelu'),
+(5, 'Muu tyyppi');
+
+ALTER TABLE Item
+ADD FOREIGN KEY (type) 
+REFERENCES ItemType(id);
+
+CREATE TABLE IF NOT EXISTS Channel (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR(128) NOT NULL,
+	author SERIAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ChannelItem (
+	item_id SERIAL,
+	channel_id SERIAL,
+	CONSTRAINT PK PRIMARY KEY (item_id,channel_id),
+	FOREIGN KEY (item_id) REFERENCES Item(id),
+	FOREIGN KEY (channel_id) REFERENCES Channel(id)
+);
+
+CREATE TABLE IF NOT EXISTS ChannelUser (
+	user_id SERIAL,
+	channel_id SERIAL,
+	isAdmin BIT,
+	isBanned BIT
+);
+  
